e4f2147c1c7f1748f090a6b1c11c759155a770bf
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-07 12:17:29 +0500

    Structure comments are addd to the addtodoview file

--- Todo App/View/AddTodoView.swift
+++ Todo App/View/AddTodoView.swift
@@ -8,10 +8,16 @@
 import SwiftUI
 
 struct AddTodoView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
+
+// MARK: - PREVIEW
 
 #Preview {
     AddTodoView()
