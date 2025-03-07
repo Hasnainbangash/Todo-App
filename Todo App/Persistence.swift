a9174d3a73e63909d214d01722d857d0828d8488
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-07 15:03:02 +0500

    Someissues get resolved

--- Todo App/Persistence.swift
+++ Todo App/Persistence.swift
@@ -13,6 +13,13 @@ struct PersistenceController {
     @MainActor
     static let preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
+        let viewContext = result.container.viewContext
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+        }
         return result
     }()
 
