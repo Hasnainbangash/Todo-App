7f5a950a32f52e164f00a24cfb6b8e664e041751
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-07 12:16:12 +0500

    Ad todo view new file is added to the project

---
+++ Todo App/View/AddTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddTodoView.swift
+//  Todo App
+//
+//  Created by Elexoft on 07/03/2025.
+//
+
+import SwiftUI
+
+struct AddTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddTodoView()
+}
