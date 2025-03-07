14067091c5c2441724f61e5cb011936352f4ce53
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-07 12:11:19 +0500

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Elexoft on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
