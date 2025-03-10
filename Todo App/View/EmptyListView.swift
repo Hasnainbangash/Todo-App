e7b235f445242dd1726445e7dc2e3ceb423f76a7
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 11:23:02 +0500

    Added a new file emptylist view with some comments

---
+++ Todo App/View/EmptyListView.swift
@@ -0,0 +1,24 @@
+//
+//  EmptyListView.swift
+//  Todo App
+//
+//  Created by Elexoft on 10/03/2025.
+//
+
+import SwiftUI
+
+struct EmptyListView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    EmptyListView()
+}
