aaa130dac04a6c7d2ee2c2760bea078629b20beb
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 14:40:04 +0500

    A new file formworlinkview is created along with the commnets

---
+++ Todo App/View/FormRowLinkView.swift
@@ -0,0 +1,24 @@
+//
+//  FormRowLinkView.swift
+//  Todo App
+//
+//  Created by Elexoft on 10/03/2025.
+//
+
+import SwiftUI
+
+struct FormRowLinkView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    FormRowLinkView()
+}
