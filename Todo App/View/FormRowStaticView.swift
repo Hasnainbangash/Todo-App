8cc4156c3ca717778363eddedeccd448dcd9cd7c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 14:09:50 +0500

    FormRowStaticView file is added to the project along with the comments are added to the project

---
+++ Todo App/View/FormRowStaticView.swift
@@ -0,0 +1,24 @@
+//
+//  FormRowStaticView.swift
+//  Todo App
+//
+//  Created by Elexoft on 10/03/2025.
+//
+
+import SwiftUI
+
+struct FormRowStaticView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    FormRowStaticView()
+}
