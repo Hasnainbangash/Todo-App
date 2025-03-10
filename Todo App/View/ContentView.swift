01b3f3fca56b4ac5747db9fea9bf41783c8c3f2c
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 12:34:19 +0500

    Adde dtha niamtion  the opacity and the scale efte of th button of the add

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     @FetchRequest(entity: Todo.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Todo.name, ascending: true)]) var todos: FetchedResults<Todo>
     
     @State private var showingAddTodoView: Bool = false
+    @State private var animatingButton: Bool = false
     
     // MARK: - BODY
     
@@ -60,13 +61,16 @@ struct ContentView: View {
                     Group {
                         Circle()
                             .fill(Color.blue)
-                            .opacity(0.2)
+                            .opacity(self.animatingButton ? 0.2 : 0)
+                            .scaleEffect(self.animatingButton ? 1 : 0)
                             .frame(width: 68, height: 68, alignment: .center)
                         Circle()
                             .fill(Color.blue)
-                            .opacity(0.15)
+                            .opacity(self.animatingButton ? 0.15 : 0)
+                            .scaleEffect(self.animatingButton ? 1 : 0)
                             .frame(width: 88, height: 88, alignment: .center)
                     }
+                    .animation(Animation.easeInOut(duration: 2).repeatForever(autoreverses: true))
                     
                     Button(action: {
                         self.showingAddTodoView.toggle()
@@ -76,6 +80,9 @@ struct ContentView: View {
                             .scaledToFit()
                             .background(Circle().fill(Color("ColorBase")))
                             .frame(width: 48, height: 48, alignment: .center)
+                    } //: BUTTON
+                    .onAppear {
+                        self.animatingButton.toggle()
                     }
                 } //: ZSTACK
                     .padding(.bottom, 15)
