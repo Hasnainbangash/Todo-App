5f53a1d9269be6b24ffc2037ac21c075298fbbf3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 12:57:14 +0500

    Settings view file is crated along ith the comments are added to the project

---
+++ Todo App/View/SettingsView.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsView.swift
+//  Todo App
+//
+//  Created by Elexoft on 10/03/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            VStack(alignment: .center, spacing: 0) {
+                // MARK: - FORM
+                
+                Form {
+                    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                } //: FORM
+                
+                // MARK: - FOOTER
+                
+                Text("Copyright © All rights reserved.\nBetter Apps ♡ Less Code")
+            } //: VSTACK
+            .navigationBarTitle("Settings", displayMode: .inline)
+        } //: NAVIGATION
+    }
+}
+
+// MARK: - PREVIEW
+
+#Preview {
+    SettingsView()
+}
