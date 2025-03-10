350d9e6499fcb511dc28ac0adabd04c871b255a6
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 14:47:11 +0500

    Added the propeties o the formrowlink view

--- Todo App/View/FormRowLinkView.swift
+++ Todo App/View/FormRowLinkView.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct FormRowLinkView: View {
     // MARK: - PROPERTIES
     
+    var icon: String
+    var color: Color
+    var text: String
+    var link: String
+    
     // MARK: - BODY
     
     var body: some View {
@@ -20,6 +25,6 @@ struct FormRowLinkView: View {
 // MARK: - PREVIEW
 
 #Preview(traits: .fixedLayout(width: 375, height: 60)) {
-    FormRowLinkView()
+    FormRowLinkView(icon: "globe", color: Color.pink, text: "Website", link: "https://swiftuimasterclass.com")
         .padding()
 }
