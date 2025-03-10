748116489fe89f655734a0c1dd5a482354564c8e
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-10 14:25:51 +0500

    FormRowStaticView design alpmsot work done

--- Todo App/View/FormRowStaticView.swift
+++ Todo App/View/FormRowStaticView.swift
@@ -10,15 +10,32 @@ import SwiftUI
 struct FormRowStaticView: View {
     // MARK: - PROPERTIES
     
+    var icon: String
+    var firstText: String
+    var secondText: String
+    
     // MARK: - BODY
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8, style: .continuous)
+                    .fill(Color.gray)
+                Image(systemName: icon)
+                    .foregroundColor(Color.white)
+            } //: ZSTACK
+            .frame(width: 36, height: 36, alignment: .center)
+            
+            Text(firstText).foregroundColor(Color.gray)
+            Spacer()
+            Text(secondText)
+        } //: HSTACK
     }
 }
 
 // MARK: - PREVIEW
 
-#Preview {
-    FormRowStaticView()
+#Preview(traits: .fixedLayout(width: 375, height: 60)) {
+    FormRowStaticView(icon: "gear", firstText: "Application", secondText: "Todo")
+        .padding()
 }
