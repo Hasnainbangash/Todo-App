24bf9f504c349cf69e8c9359e8a48d7207000e06
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-03-11 13:15:31 +0500

    Theme data model created and work done

---
+++ Todo App/Theme/ThemeModel.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeModel.swift
+//  Todo App
+//
+//  Created by Elexoft on 11/03/2025.
+//
+
+import SwiftUI
+
+// MARK: - THEME MODEL
+
+struct Theme: Identifiable {
+    let id: Int
+    let themeName: String
+    let themeColor: Color
+}
